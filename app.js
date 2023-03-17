9967735b05d53fa95a4733aa551962170880e17c
Kelvin-Kamau999 <76435357+Kelvin-Kamau999@users.noreply.github.com>
2023-03-17 17:59:09 +0300

    1
    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const cart = document.querySelector("nav .cart")
+const cartSideBar = document.querySelector(".cart-Sidebar")
+const closeCart = document.querySelector(".close-cart")
+const burger = document.querySelector(".burger")
+const menuSidebar = document.querySelector(".menu-sidebar")
+const closeMenu = document.querySelector(".close-menu")
+const cartItemsTotal = document.querySelector("noi")
+const cartPriceTotal = document.querySelector(".total-amount")
+const cartUi = document.querySelector(".cart-sidebar .cart")
+const totalDiv = document.querySelector(".total-sum")
+const clearBtn = document.querySelector(".clear-cart-btn")
+const cartContent = document.querySelector(".cart-content")
+
+
+let Cart=[];
+let buttonsDOM=[];
+
+cart.addEventListener("click",function(){
+    cartSideBar.style.transform= "translate(0%)"
+    const bodyOverlay =document.createElement("div")
+    bodyOverlay.classList.add("overlay");
+    setTimeout(function(){
+        document.querySelector("body").append(bodyOverlay)
+    },300)
+})
+
+closeCart.addEventListener("click", function(){
+    cartSideBar.style.transform = "translate(100%)";
+    const bodyOverlay = document.querySelector(".overlay")
+    document.querySelector("body").removeChild(bodyOverlay)
+})
